16ea93ae51615030ac37d4cef72c7df881697365
zdenham <zdenham1@gmail.com>
2022-12-16 15:17:54 -0800

    initial erc721

--- contracts/facets/ERC721/ERC721Facet.sol
+++ contracts/facets/ERC721/ERC721Facet.sol
@@ -3,121 +3,25 @@
 pragma solidity ^0.8.0;
 
 import {IERC721Events} from "./IERC721Events.sol";
+import {ERC721Lib} from "./ERC721Lib.sol";
 
 contract ERC721Facet is IERC721Events {
-    // Token name
-    string private _name;
-
-    // Token symbol
-    string private _symbol;
-
-    // Mapping from token ID to owner address
-    mapping(uint256 => address) private _owners;
-
-    // Mapping owner address to token count
-    mapping(address => uint256) private _balances;
-
-    // Mapping from token ID to approved address
-    mapping(uint256 => address) private _tokenApprovals;
-
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-
-    /**
-     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
-     */
-    constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    /**
-     * @dev See {IERC721-balanceOf}.
-     */
-    function balanceOf(address owner)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
-        require(
-            owner != address(0),
-            "ERC721: address zero is not a valid owner"
-        );
-        return _balances[owner];
+    function balanceOf(address owner) public view virtual returns (uint256) {
+        return ERC721Lib.balanceOf(owner);
     }
 
     /**
      * @dev See {IERC721-ownerOf}.
      */
-    function ownerOf(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (address)
-    {
-        address owner = _ownerOf(tokenId);
-        require(owner != address(0), "ERC721: invalid token ID");
-        return owner;
-    }
-
-    /**
-     * @dev See {IERC721Metadata-name}.
-     */
-    function name() public view virtual override returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev See {IERC721Metadata-symbol}.
-     */
-    function symbol() public view virtual override returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @dev See {IERC721Metadata-tokenURI}.
-     */
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        _requireMinted(tokenId);
-
-        string memory baseURI = _baseURI();
-        return
-            bytes(baseURI).length > 0
-                ? string(abi.encodePacked(baseURI, tokenId.toString()))
-                : "";
-    }
-
-    /**
-     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
-     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
-     * by default, can be overridden in child contracts.
-     */
-    function _baseURI() internal view virtual returns (string memory) {
-        return "";
+    function ownerOf(uint256 tokenId) public view virtual returns (address) {
+        return ERC721Lib.ownerOf(tokenId);
     }
 
     /**
      * @dev See {IERC721-approve}.
      */
-    function approve(address to, uint256 tokenId) public virtual override {
-        address owner = ERC721.ownerOf(tokenId);
-        require(to != owner, "ERC721: approval to current owner");
-
-        require(
-            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
-            "ERC721: approve caller is not token owner or approved for all"
-        );
-
-        _approve(to, tokenId);
+    function approve(address to, uint256 tokenId) public virtual {
+        return ERC721Lib.approve(to, tokenId);
     }
 
     /**
@@ -127,23 +31,16 @@ contract ERC721Facet is IERC721Events {
         public
         view
         virtual
-        override
         returns (address)
     {
-        _requireMinted(tokenId);
-
-        return _tokenApprovals[tokenId];
+        return ERC721Lib.getApproved(tokenId);
     }
 
     /**
      * @dev See {IERC721-setApprovalForAll}.
      */
-    function setApprovalForAll(address operator, bool approved)
-        public
-        virtual
-        override
-    {
-        _setApprovalForAll(_msgSender(), operator, approved);
+    function setApprovalForAll(address operator, bool approved) public virtual {
+        return ERC721Lib.setApprovalForAll(operator, approved);
     }
 
     /**
@@ -153,10 +50,9 @@ contract ERC721Facet is IERC721Events {
         public
         view
         virtual
-        override
         returns (bool)
     {
-        return _operatorApprovals[owner][operator];
+        return ERC721Lib.isApprovedForAll(owner, operator);
     }
 
     /**
@@ -166,14 +62,8 @@ contract ERC721Facet is IERC721Events {
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override {
-        //solhint-disable-next-line max-line-length
-        require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "ERC721: caller is not token owner or approved"
-        );
-
-        _transfer(from, to, tokenId);
+    ) public virtual {
+        return ERC721Lib.transferFrom(from, to, tokenId);
     }
 
     /**
@@ -183,8 +73,8 @@ contract ERC721Facet is IERC721Events {
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override {
-        safeTransferFrom(from, to, tokenId, "");
+    ) public virtual {
+        return ERC721Lib.safeTransferFrom(from, to, tokenId);
     }
 
     /**
@@ -195,5 +85,7 @@ contract ERC721Facet is IERC721Events {
         address to,
         uint256 tokenId,
         bytes memory data
-    ) public virtual override {}
+    ) public virtual {
+        return ERC721Lib.safeTransferFrom(from, to, tokenId, data);
+    }
 }
